68296e0f84cb2346ffd7c2879d7b4d81deec337e
itsazahara <aromsch1403@g.educaand.es>
2025-05-23 13:58:43 +0200

    Se ha modificado la página de inicio de la web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes, Scroll } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MenuUsuarioComponent } from './pages/menu_usuario/menu_usuario.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
@@ -20,32 +20,52 @@ import { DetallesReservaComponent } from './pages/detalles-reserva/detalles-rese
 import { ReservasPersonalesComponent } from './pages/reservas-personales/reservas-personales.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { PreguntasFrecuentesComponent } from './pages/preguntas-frecuentes/preguntas-frecuentes.component';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs';
 
 const routes: Routes = [
-  { path: 'menu_usuario', component: MenuUsuarioComponent},
-  { path: 'actualizar_usuario', component: ActualizarUsuarioComponent},
+  { path: 'menu_usuario', component: MenuUsuarioComponent },
+  { path: 'actualizar_usuario', component: ActualizarUsuarioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'raza', component: RazaComponent },
   { path: 'mascotas-por-raza/:id', component: MascotasPorRazaComponent },
-  { path: 'menu_administrador', component: MenuAdministradorComponent},
-  { path: 'add_mascota', component: AddMascotaComponent},
+  { path: 'menu_administrador', component: MenuAdministradorComponent },
+  { path: 'add_mascota', component: AddMascotaComponent },
   { path: 'selector-rol', component: SelectorRolComponent },
   { path: 'login-cliente', component: LoginClienteComponent },
   { path: 'login-admin', component: LoginAdministradorComponent },
   { path: 'mascotas', component: MascotasComponent },
   { path: 'adoptar_mascota/:id', component: InformacionMascotaComponent },
   { path: 'registro', component: RegistroClienteComponent },
-  { path: 'datos_administrador', component: DatosAdministradorComponent},
-  { path: 'reservas', component: ReservasComponent},
+  { path: 'datos_administrador', component: DatosAdministradorComponent },
+  { path: 'reservas', component: ReservasComponent },
   { path: 'detalles-reserva/:id', component: DetallesReservaComponent },
-  { path: 'reservas-personales/:id', component: ReservasPersonalesComponent},
-  { path: 'about-us', component: AboutUsComponent},
-  { path: 'preguntas-frecuentes', component: PreguntasFrecuentesComponent},
+  { path: 'reservas-personales/:id', component: ReservasPersonalesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'preguntas-frecuentes', component: PreguntasFrecuentesComponent },
   { path: '', component: InicioComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(router: Router, viewportScroller: ViewportScroller) {
+    router.events.pipe(
+      filter((e): e is Scroll => e instanceof Scroll)
+    ).subscribe(e => {
+      if (e.position) {
+        viewportScroller.scrollToPosition(e.position);
+      } else if (e.anchor) {
+      } else {
+        viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
+}
+
+
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+
+  constructor(private router: Router) {}
+
+  verMascotasPorEspecie(especie: string): void {
+    this.router.navigate(['/mascotas'], { queryParams: { especie: especie } });
+  }
 
 }
